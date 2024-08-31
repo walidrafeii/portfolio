ac10cf4a0afc90d45d33d5594a83582fcb120dc7
Walid Rafei <wrafei@outlook.com>
2024-08-30 17:55:08 -0700

    Improve Experience section and support on smaller screen

--- app/components/experience/ExperienceTable.tsx
+++ app/components/experience/ExperienceTable.tsx
@@ -1,10 +1,13 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export interface ExperienceEntry {
   id: number;
   position: string;
   title: string;
+  startDate: string;
+  endDate: string;
+  companyLink: string;
   points: string[];
 }
 
@@ -12,14 +15,26 @@ export const ExperienceTable: React.FC<{ experiences: ExperienceEntry[] }> = ({
   experiences,
 }) => {
   // State Management
+  const [isDetailsFadingOut, setIsDetailsFadingOut] = useState(false);
   const scrollBarHeight = useRef<HTMLDivElement>(null); // reference to scroll bar height
   const [selectedExperienceIndex, setSelectedExperienceIndex] = useState<
     number | null
   >(0);
 
   const handleExperienceClick = (index: number) => {
-    setSelectedExperienceIndex(index);
+    setIsDetailsFadingOut(true); // Initiate fade-out before changing index
+    setTimeout(() => {
+      setSelectedExperienceIndex(index);
+      setIsDetailsFadingOut(false); // Reset fade-out state after transition
+    }, 200);
   };
+
+  // Handle fade-in/out animation on state change
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setIsDetailsFadingOut(false), 200);
+
+    return () => clearTimeout(timeoutId); // Cleanup on unmount
+  }, [selectedExperienceIndex, isDetailsFadingOut]);
 
   /**
    * Calculates the current top position of the scroll bar based on selected experience index.
@@ -32,16 +47,15 @@ export const ExperienceTable: React.FC<{ experiences: ExperienceEntry[] }> = ({
     const fullHeight = scrollBarHeight.current?.clientHeight || 0;
     const scrollHeight = fullHeight / experiences.length;
     const topPosition = scrollHeight * selectedExperienceIndex;
-    console.log(`top-[${topPosition}px]`);
     return `top-[${topPosition}px]`;
   };
 
   return (
-    <div className="relative mx-auto w-fit mt-10">
-      <div className="flex flex-row gap-2">
+    <div className="flex flex-col items-center sm:flex-row sm:items-start justify-center md:w-[80%] mx-auto w-full mt-10 gap-12">
+      <div className="flex flex-row gap-2 w-fit h-fit">
         {/* Scroll Bar */}
         <div
-          className="relative flex-grow w-[2px] bg-navy-lightest rounded-full"
+          className="relative w-[2px] bg-navy-lightest rounded-full"
           ref={scrollBarHeight}
         >
           <div
@@ -54,29 +68,42 @@ export const ExperienceTable: React.FC<{ experiences: ExperienceEntry[] }> = ({
           {experiences.map((experience, index) => (
             <ul
               key={index}
-              className="min-w-[180px] text-md text-slate-light py-2 px-4 hover:bg-navy-lightest hover:cursor-pointer rounded-md transition duration-300 ease-in-out"
+              className={`min-w-[180px] text-sm text-slate-light mt-1 py-2 px-4 
+                hover:bg-navy-lightest hover:cursor-pointer rounded-md transition duration-300 ease-in-out ${
+                  index === selectedExperienceIndex ? "bg-navy-lightest" : ""
+                }`}
               onClick={() => handleExperienceClick(index)}
             >
               {experience.title}
             </ul>
           ))}
         </div>
-        {/* Selected experience details (conditional rendering) */}
-        {selectedExperienceIndex !== null && (
-          <div className="flex flex-col ml-auto bg-gray-100 rounded-md shadow-md px-4 py-2 transition-opacity duration-500 ease-in-out">
-            <h3 className="text-lg font-medium mb-2">
-              {experiences[selectedExperienceIndex].title}
-            </h3>
-            <ul className="list-disc space-y-1">
-              {experiences[selectedExperienceIndex].points.map(
-                (point, pointIndex) => (
-                  <li key={pointIndex}>{point}</li>
-                )
-              )}
-            </ul>
-          </div>
-        )}
       </div>
+      {/* Selected experience details (conditional rendering) */}
+      {selectedExperienceIndex !== null && (
+        <div
+          className={`flex flex-col text-slate flex-grow transition-opacity duration-500 ease-in-out ${
+            isDetailsFadingOut ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          <h3 className="font-semibold mb-2 text-slate-lightest text-lg">
+            {experiences[selectedExperienceIndex].title} -{" "}
+            {experiences[selectedExperienceIndex].position}
+          </h3>
+          <p className="text-sm text-slate-light">
+            {" "}
+            {experiences[selectedExperienceIndex].startDate} -{" "}
+            {experiences[selectedExperienceIndex].endDate}
+          </p>
+          <ul className="list-disc space-y-3 mt-6 text-sm text-slate-light">
+            {experiences[selectedExperienceIndex].points.map(
+              (point, pointIndex) => (
+                <li key={pointIndex}>{point}</li>
+              )
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
